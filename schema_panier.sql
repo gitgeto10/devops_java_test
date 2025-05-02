4f1f7aef64972ee2b81835ad8aebd5a2f6ea5864
YASSMINE <yassmine.aithoussa.41@edu.uiz.ac.ma>
2025-05-02 21:28:44 +0100

    Panier

--- schema_panier.sql
+++ schema_panier.sql
@@ -1,13 +1,7 @@
 -- Création de la table panier
 CREATE TABLE IF NOT EXISTS panier (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    article VARCHAR(255) NOT NULL,
+    n_article VARCHAR(255) NOT NULL,
     client VARCHAR(255) NOT NULL
 );
 
--- Insertion d'exemples de données dans la table panier
-INSERT INTO panier (article, client) VALUES 
-('ART001', 'Ahmed'),
-('ART002', 'Jamila'),
-('ART003', 'SAAD'),
-('ART004', 'SAMIRA');
